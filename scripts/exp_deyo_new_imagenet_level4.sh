5c6b0c34e4473b155a61a305cf36c7b181ee3d6d
NgTMDuc <ducntm23052002@gmail.com>
2024-09-11 20:59:46 +0700

    Update scripts

--- scripts/exp_deyo_new_imagenet_level4.sh
+++ scripts/exp_deyo_new_imagenet_level4.sh
@@ -1,17 +1,17 @@
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.1
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.1
 
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.2
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.2
 
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.3
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.3
 
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.4
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.4
 
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.5
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.5
 
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.6
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.6
 
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.7
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.7
 
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.8
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.8
 
-python3 main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.9
+python3 ./main_new.py --model vitbase_timm --exp_type normal --level 4 --method deyo --num_sim 900 --alpha_cap 0.9
